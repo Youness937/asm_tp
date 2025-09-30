e0ac422e6f56c7ecfd4cc39c5825f91aad534334
Youness Parbotteea <youness.parbotteea@gmail.com>
2025-09-30 16:29:04 +0200

    asm05 correction

--- asm05/asm05.s
+++ asm05/asm05.s
@@ -2,11 +2,16 @@ section .text
     global _start
 
 _start:
-    ; Récupérer argv[1]
-    mov rbx, [rsp+16]       ; rbx = pointeur vers argv[1]
+    ; Charger argc correctement (32 bits sur la pile)
+    mov eax, [rsp]          ; eax = argc
+    cmp eax, 2              ; si argc < 2 (pas d'argument)
+    jl .no_arg
+
+    ; Récupérer argv[1] (pointeur)
+    mov rbx, [rsp+16]
 
     ; Calculer strlen(argv[1])
-    mov rcx, rbx            ; rcx = pointeur courant
+    mov rcx, rbx
 .len_loop:
     cmp byte [rcx], 0
     je .len_done
@@ -16,14 +21,21 @@ _start:
 .len_done:
     sub rcx, rbx            ; rcx = longueur de la chaîne
 
-    ; Appeler write(1, argv[1], longueur)
-    mov rax, 1              ; syscall write
+    ; write(1, argv[1], longueur)
+    mov rax, 1              ; syscall: write
     mov rdi, 1              ; fd = stdout
     mov rsi, rbx            ; buffer = argv[1]
-    mov rdx, rcx            ; taille
+    mov rdx, rcx            ; taille = longueur
     syscall
 
     ; exit(0)
-    mov rax, 60             ; syscall exit
+    mov rax, 60             ; syscall: exit
     xor rdi, rdi            ; code = 0
     syscall
+
+.no_arg:
+    ; exit(1) si pas d'argument
+    mov rax, 60
+    mov rdi, 1
+    syscall
+
