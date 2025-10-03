d9966c3bce24561b7f01a85fe5bc0be18079f03f
Youness Parbotteea <youness.parbotteea@gmail.com>
2025-10-03 12:20:33 +0200

    asm17 corr

--- asm17/asm17.s
+++ asm17/asm17.s
@@ -1,23 +1,29 @@
 section .bss
-    buffer resb 128          ; tampon pour lire la chaîne
+    buffer resb 128          ; tampon pour la chaîne lue
 
 section .text
     global _start
 
 _start:
-    ; --- récupérer l’argument argv[1] (le shift) ---
-    mov rsi, [rsp+16]        ; argv[1]
-    movzx rdi, byte [rsi]    ; premier caractère (ex: '3')
-    sub rdi, '0'             ; convertir ASCII -> int
-    mov r12, rdi             ; garder le shift dans R12
+    ; --- récupérer argc ---
+    mov rdi, [rsp]           ; argc
+    cmp rdi, 2               ; argc < 2 ?
+    jl .error                ; si pas d’argument -> erreur
 
+    ; --- récupérer argv[1] ---
+    mov rsi, [rsp+16]        ; argv[1]
+    movzx rdi, byte [rsi]    ; premier caractère de argv[1]
+    sub rdi, '0'             ; convertir ASCII -> entier
+    mov r12, rdi             ; sauvegarder le shift dans R12
+
+.read_input:
     ; --- lecture depuis stdin ---
     mov rax, 0               ; syscall read
     mov rdi, 0               ; stdin
     mov rsi, buffer
     mov rdx, 128
     syscall
-    mov rcx, rax             ; rcx = nb de caractères lus
+    mov rcx, rax             ; rcx = nb caractères lus
 
     ; --- boucle sur chaque caractère ---
     xor rbx, rbx             ; index = 0
@@ -30,17 +36,17 @@ _start:
 
     ; ---------- minuscules ----------
     cmp al, 'a'
-    jl .check_upper           ; si < 'a', aller tester maj
+    jl .check_upper
     cmp al, 'z'
-    jg .check_upper           ; si > 'z', aller tester maj
+    jg .check_upper
 
-    sub al, 'a'              ; ramener dans [0–25]
-    add al, r12b             ; ajouter le shift
+    sub al, 'a'
+    add al, r12b
     xor rdx, rdx
     movzx rax, al
     mov rcx, 26
-    div rcx                  ; quotient dans RAX, reste dans RDX
-    mov al, dl               ; al = reste
+    div rcx
+    mov al, dl
     add al, 'a'
     jmp .store
 
@@ -61,19 +67,25 @@ _start:
     add al, 'A'
 
 .store:
-    mov [buffer+rbx], al     ; écrire le caractère transformé
+    mov [buffer+rbx], al
     inc rbx
     jmp .loop
 
 .done:
-    ; --- écrire résultat sur stdout ---
+    ; --- écrire le résultat ---
     mov rax, 1
     mov rdi, 1
     mov rsi, buffer
     mov rdx, rcx
     syscall
 
-    ; --- quitter ---
+    ; --- quitter proprement ---
     mov rax, 60
     xor rdi, rdi
+    syscall
+
+.error:
+    ; --- pas assez d’arguments ---
+    mov rax, 60              ; syscall exit
+    mov rdi, 1               ; code erreur = 1
     syscall
